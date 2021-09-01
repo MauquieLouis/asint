59c5607fa91171065cc5f8f1a0eecc31fa8bed8e
MauquieLouis <louis.mauquie@telecom-sudparis.eu>
2021-09-01 14:43:44 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,53 @@
+<?php  
+// src/Controller/HomeController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(){
+        //Function
+        return $this->render('asint/home.html.twig',[]);
+    }
+    
+    /**
+     * @Route("/sports", name="sports")
+     */
+    public function sports(){
+        
+        return $this->render('asint/sports.html.twig',[]);
+    }
+    
+    /**
+     * @Route("/evenements", name="evenements")
+     */
+    public function evenements(){
+        
+        return $this->render('asint/evenements.html.twig',[]);
+    }
+    
+    /**
+     * @Route("/partenaires", name="partenaires")
+     */
+    public function partenaires(){
+        
+        return $this->render('asint/partenaires.html.twig',[]);
+    }
+    
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(){
+        
+        return $this->render('asint/contact.html.twig',[]);
+    }
+}
+
+?>
